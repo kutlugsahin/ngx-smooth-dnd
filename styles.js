0d52d6ca4050f579265d57ba3633bc8e5be55d1e
ahmet kutlu sahin <kutlugsahin@gmail.com>
2018-08-13 19:41:21 +0200

    Updates

--- styles.js
+++ styles.js
@@ -539,7 +539,7 @@ if(false) {}
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! C:\Users\ksahin\Desktop\ngx-smooth-dnd\src\styles.css */"./src/styles.css");
+module.exports = __webpack_require__(/*! C:\Users\kutlu\Desktop\ngx-smooth-dnd\src\styles.css */"./src/styles.css");
 
 
 /***/ })
